aae7eaa9207cf316f2fb88d944f1ebda3ca51ad4
Joscha Germerott <joscha.germerott@googlemail.com>
2024-01-29 21:01:39 +0100

    Visual improvements on the issues page

--- PirateTools.TravelExpense.WebApp/Pages/Wizard/StepEntryIssues.razor.cs
+++ PirateTools.TravelExpense.WebApp/Pages/Wizard/StepEntryIssues.razor.cs
@@ -1,3 +1,4 @@
+using PirateTools.Models;
 using PirateTools.TravelExpense.WebApp.Models;
 
 namespace PirateTools.TravelExpense.WebApp.Pages.Wizard;
@@ -16,5 +17,21 @@ public partial class StepEntryIssues {
         } else {
             NavigationManager.NavigateTo("StepBankInformationEntry");
         }
+    }
+
+    private string ClassForSeverity(ReportIssueSeverity severity) {
+        return severity switch {
+            ReportIssueSeverity.Warning => "warning",
+            ReportIssueSeverity.Error => "danger",
+            _ => ""
+        };
+    }
+
+    private string IconForSeverity(ReportIssueSeverity severity) {
+        return severity switch {
+            ReportIssueSeverity.Warning => "bi bi-exclamation-circle-fill",
+            ReportIssueSeverity.Error => "bi bi-exclamation-octagon-fill",
+            _ => ""
+        };
     }
 }

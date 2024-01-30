065d59e39aa14b43e227d4917f003efe5a95fbe5
Joscha Germerott <joscha.germerott@googlemail.com>
2024-01-30 21:14:18 +0100

    Improve action buttons on home page to be more accessible

---
+++ PirateTools.Web.Common/Bootstrap/BootstrapType.cs
@@ -0,0 +1,30 @@
+﻿namespace PirateTools.Web.Common.Bootstrap;
+
+public enum BootstrapType {
+    None,
+    Primary,
+    Secondary,
+    Success,
+    Danger,
+    Warning,
+    Info,
+    Light,
+    Dark
+}
+
+public static class BootstrapTyoeExtensions {
+    public static string ToCssClass(this BootstrapType type) {
+        return type switch {
+            BootstrapType.None => "",
+            BootstrapType.Primary => "primary",
+            BootstrapType.Secondary => "secondary",
+            BootstrapType.Success => "success",
+            BootstrapType.Danger => "danger",
+            BootstrapType.Warning => "warning",
+            BootstrapType.Info => "info",
+            BootstrapType.Light => "light",
+            BootstrapType.Dark => "dark",
+            _ => ""
+        };
+    }
+}
---
+++ PirateTools.Web.Common/Bootstrap/Button.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PirateTools.Web.Common.Bootstrap;
+
+public partial class Button : ComponentBase {
+    [Parameter]
+    public EventCallback OnClick { get; set; }
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+    [Parameter]
+    public required string Title { get; set; }
+    [Parameter]
+    public BootstrapType Type { get; set; }
+    [Parameter]
+    public bool Small { get; set; }
+}
